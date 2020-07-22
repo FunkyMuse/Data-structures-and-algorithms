d788769163b61739c7f388992cdfc65c093f9067
CraZyLegenD <crazyxylene@gmail.com>
2020-07-22 12:04:27 +0200

    queue reversing Extensions.kt

---
+++ src/queue/Extensions.kt
@@ -0,0 +1,17 @@
+package queue
+
+import java.util.*
+
+fun <T> Queue<T>.reverse() {
+    val stack = Stack<T>()
+    var next = this.dequeue()
+    while (next != null) {
+        stack.push(next)
+        next = this.dequeue()
+    }
+    next = stack.pop()
+    while (next != null) {
+        this.enqueue(next)
+        next = stack.pop()
+    }
+}
