bf14faaa78f9dffd7830af0a55aba3a6945ad117
CraZyLegenD <crazyxylene@gmail.com>
2020-08-04 19:03:12 +0200

    heap

---
+++ src/heap/Heap.kt
@@ -0,0 +1,10 @@
+package heap
+
+interface Heap<T>:Collection<T> {
+    fun peek():T?
+    fun getNthSmallestT(n: T): T?
+
+    fun merge(heap: AbstractHeap<T>)
+
+    fun isMinHeap(): Boolean
+}
--- src/main.kt
+++ src/main.kt
@@ -1,18 +1,14 @@
-import binarySearch.binarySearch
-import binarySearch.findIndices
+import heap.ComparableHeap
+import heap.ComparatorHeap
 
 fun main() {
-    "binary search" example {
-        val array = arrayListOf(1, 5, 15, 17, 19, 22, 24, 31, 105,
-                150)
-        val search31 = array.indexOf(150)
-        val binarySearch31 = array.binarySearch(150)
-        println("indexOf(): $search31")
-        println("binarySearch(): $binarySearch31")
+    val array = arrayListOf(3, 10, 18, 5, 21, 100)
+    val inverseComparator = Comparator<Int> { o1, o2 ->
+        o2.compareTo(o1)
     }
-
-    val array = arrayListOf(1, 2, 3, 3, 3, 4, 5, 5)
-    val indices = array.findIndices(3)
-    println(indices)
+    val minHeap = ComparatorHeap.create(array, inverseComparator)
+    val maxHeap = ComparableHeap.create(array)
+    println(minHeap.isMinHeap())
+    println(maxHeap.isMinHeap())
 }
 
