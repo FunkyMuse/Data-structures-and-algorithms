a8abfe8877dc0906c32c41572fafe60b77ff9466
CraZyLegenD <crazyxylene@gmail.com>
2020-08-31 23:05:20 +0200

    number of paths calculations

--- src/graphs/Graph.kt
+++ src/graphs/Graph.kt
@@ -4,19 +4,47 @@ package graphs
  * Created by crazy on 8/31/20 to long live and prosper !
  */
 interface Graph<T> {
-    fun createVertex(data: T): Vertex<T>
-    fun addDirectedEdge(source: Vertex<T>,
-                        destination: Vertex<T>,
-                        weight: Double?)
 
-    fun addUndirectedEdge(source: Vertex<T>,
-                          destination: Vertex<T>,
-                          weight: Double?)
-    fun add(edge: EdgeType,
-            source: Vertex<T>,
-            destination: Vertex<T>,
-            weight: Double?)
+    fun createVertex(data: T): Vertex<T>
+
+    fun addDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?)
+
+    fun addUndirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        addDirectedEdge(source, destination, weight)
+        addDirectedEdge(destination, source, weight)
+    }
+
+    fun add(edge: EdgeType, source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        when (edge) {
+            EdgeType.DIRECTED -> addDirectedEdge(source, destination, weight)
+            EdgeType.UNDIRECTED -> addUndirectedEdge(source, destination, weight)
+        }
+    }
+
     fun edges(source: Vertex<T>): ArrayList<Edge<T>>
-    fun weight(source: Vertex<T>,
-               destination: Vertex<T>): Double?
+
+    fun weight(source: Vertex<T>, destination: Vertex<T>): Double?
+
+    fun numberOfPaths(source: Vertex<T>, destination: Vertex<T>): Int {
+        val numberOfPaths = NumberOfPaths(0)
+        val visited: MutableSet<Vertex<T>> = mutableSetOf()
+        paths(source, destination, visited, numberOfPaths)
+        return numberOfPaths.value
+    }
+
+    fun paths(source: Vertex<T>, destination: Vertex<T>, visited: MutableSet<Vertex<T>>, pathCount: NumberOfPaths<Int>) {
+        visited.add(source)
+        if (source == destination) {
+            pathCount.value += 1
+        } else {
+            val neighbors = edges(source)
+            neighbors.forEach { edge ->
+                if (edge.destination !in visited) {
+                    paths(edge.destination, destination, visited, pathCount)
+                }
+            }
+        }
+        visited.remove(source)
+    }
+
 }
---
+++ src/graphs/NumberOfPaths.kt
@@ -0,0 +1,7 @@
+package graphs
+
+/**
+ * Created by crazy on 8/31/20 to long live and prosper !
+ */
+
+data class NumberOfPaths<T>(var value: T)
